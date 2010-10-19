5e2dda0936b38e4cf9f5f45ff203ce2020015c28
Patrick Turley <patrick.turley@gmail.com>
2010-10-19 00:39:03 -0500

    Made socket server start on webserver boot

--- app/websocket_server/server.rb
+++ app/websocket_server/server.rb
@@ -2,30 +2,35 @@ require 'rubygems'
 require 'eventmachine'
 require 'em-websocket'
 
-EM.run {
-  websocket_connections = []
+class SetlistWebsocketServer
+  def self.start
+    fork do
+      EM.run {
+        websocket_connections = []
   
-  EM::WebSocket.start(:host => "10.2.12.38", :port => 8080) do |ws|
-    ws.onopen {
-      ws.send "Hello Client!"
-      puts "Websocket connection opened"
-      websocket_connections << ws
-      puts "The sockets are:"
-      websocket_connections.each do |socket|
-        puts socket.request["Origin"]
-      end
-    }
+        EM::WebSocket.start(:host => "10.2.12.38", :port => 8080) do |ws|
+          ws.onopen {
+            ws.send "Hello Client!"
+            puts "Websocket connection opened"
+            websocket_connections << ws
+            puts "The sockets are:"
+            websocket_connections.each do |socket|
+              puts socket.request["Origin"]
+            end
+          }
     
-    ws.onmessage do |msg| 
-      websocket_connections.each do |socket|
-        socket.send "#{msg}"
-      end 
+          ws.onmessage do |msg| 
+            websocket_connections.each do |socket|
+              socket.send "#{msg}"
+            end 
+          end
+    
+          ws.onclose {
+            puts "Websocket connection closed"
+            websocket_connections.delete(ws)
+          }
+        end
+      }
     end
-    
-    ws.onclose {
-      puts "Websocket connection closed"
-      websocket_connections.delete(ws)
-    }
   end
-  
-}
+end
